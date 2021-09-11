c3f615c852bb89fc7e34c2d7df0ac32848b6f951
Samir Bittar <samir@matchboxexchange.com>
2021-09-11 14:23:40 +1000

    Initial commit.

---
+++ JBHiFi.Samir/Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JBHiFi.Samir.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+    }
+}
